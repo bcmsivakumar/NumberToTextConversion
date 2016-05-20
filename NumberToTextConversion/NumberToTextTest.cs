e05f4901152d2c39d6e4948d0386cdc3922e6197
Sivakumar <bcm.sivakumar@gmail.com>
2016-05-20 20:16:24 +0530

    Created new project number to text conversion

---
+++ NumberToTextConversion/NumberToTextTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberTextConvertors;
+
+namespace NumberToTextConversionTests
+{
+    [TestClass]
+    public class NumberToTextTest
+    {
+        [TestMethod]
+        public void NumberToText_SingleDigit_One()
+        {
+            NumberTextConverter converter = new NumberTextConverter();
+            string numberInText = converter.ConvertToText(1);
+
+            Assert.AreEqual("One", numberInText);
+        }
+    }
+}
---
+++ TextConvertor/NumberTextConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTextConvertors
+{
+    public class NumberTextConverter
+    {
+        public string ConvertToText(int number)
+        {
+            return "One";
+        }
+    }
+}
